79ee66082a9a70b46c23538f52d470fcccc846ee
Muhammad-Almujtaba Yaseen <110176158+Almujtaba-Yaseen@users.noreply.github.com>
2024-05-22 09:14:56 +0200

    Update Newton.py

--- Newton.py
+++ Newton.py
@@ -2,16 +2,22 @@ import math
 
 # Define the function representing the mathematical function you want to solve using the Newton's algorithm, which is Kepler's equation: E - 0.01*sin(E) = M
 def f(x):
-    M = 3
+    M = 7 #change this value as needed
     f_x = x - 0.01 * math.sin(x) - M
+
     return f_x
+
+
 
 # Define the function for estimating derivatives
 def derivative_f(x):
     h = 0.001
     x_plus_h = x + h
     df_x = (f(x_plus_h) - f(x)) / h
+    
     return df_x
+
+
 
 # Implement the Newton's algorithm
 def newton():
